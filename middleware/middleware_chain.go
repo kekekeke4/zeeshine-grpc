b35ddaca64d66420c7d17d661e3362893538e7f7
zengke <kez@zengkedeMac-mini.local>
2019-03-19 09:57:50 +0800

    添加panic中间件对stream的支持

--- middleware/middleware_chain.go
+++ middleware/middleware_chain.go
@@ -1,54 +1,55 @@
 package middleware
 
-import(
+import (
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	"google.golang.org/grpc"
 )
 
-type MiddlewareChainOptionsAction func(mco *MiddlewareChainOptions) (grpc.UnaryServerInterceptor,grpc.StreamServerInterceptor)
+type MiddlewareChainOptionsAction func(mco *MiddlewareChainOptions) (grpc.UnaryServerInterceptor, grpc.StreamServerInterceptor)
 
-type MiddlewareChainOptions struct{
-	unaryInters []grpc.UnaryServerInterceptor
+type MiddlewareChainOptions struct {
+	unaryInters  []grpc.UnaryServerInterceptor
 	streamInters []grpc.StreamServerInterceptor
 }
 
-func NewMiddlewareChainOptions() *MiddlewareChainOptions{
+func NewMiddlewareChainOptions() *MiddlewareChainOptions {
 	return &MiddlewareChainOptions{
-		unaryInters:make([]grpc.UnaryServerInterceptor,0,10),
+		unaryInters: make([]grpc.UnaryServerInterceptor, 0, 10),
 	}
 }
 
-func (mo *MiddlewareChainOptions)AddUnary(delegate UnaryRequestDelegate) *MiddlewareChainOptions{
-	inter:=grpc.UnaryServerInterceptor(delegate)
-	mo.unaryInters = append(mo.unaryInters,inter)
+func (mo *MiddlewareChainOptions) AddUnary(delegate UnaryRequestDelegate) *MiddlewareChainOptions {
+	inter := grpc.UnaryServerInterceptor(delegate)
+	mo.unaryInters = append(mo.unaryInters, inter)
 	return mo
 }
 
-func (mo *MiddlewareChainOptions)AddStream(delegate StreamRequestDelegate) *MiddlewareChainOptions {
-	inter:=grpc.StreamServerInterceptor(delegate)
-	mo.streamInters = append(mo.streamInters,inter)
+func (mo *MiddlewareChainOptions) AddStream(delegate StreamRequestDelegate) *MiddlewareChainOptions {
+	inter := grpc.StreamServerInterceptor(delegate)
+	mo.streamInters = append(mo.streamInters, inter)
 	return mo
 }
 
-func (mo *MiddlewareChainOptions)UsePaincMiddleware() *MiddlewareChainOptions {
-	return mo.AddUnary(handlePainc)
+func (mo *MiddlewareChainOptions) UsePaincMiddleware() *MiddlewareChainOptions {
+	return mo.AddUnary(handlePainc).
+		AddStream(hanlderStreamPainc)
 }
 
-func (mo *MiddlewareChainOptions)UseOAuthMiddleware() *MiddlewareChainOptions {
+func (mo *MiddlewareChainOptions) UseOAuthMiddleware() *MiddlewareChainOptions {
 	return mo.AddUnary(handleOAuth)
 }
 
-func(mo *MiddlewareChainOptions) BuildMiddleware()(grpc.UnaryServerInterceptor,grpc.StreamServerInterceptor){
+func (mo *MiddlewareChainOptions) BuildMiddleware() (grpc.UnaryServerInterceptor, grpc.StreamServerInterceptor) {
 	var unary grpc.UnaryServerInterceptor
-	var stream grpc.StreamServerInterceptor 
+	var stream grpc.StreamServerInterceptor
 
-	if len(mo.unaryInters)>0 {
-		unary = grpc_middleware.ChainUnaryServer(mo.unaryInters...) 
+	if len(mo.unaryInters) > 0 {
+		unary = grpc_middleware.ChainUnaryServer(mo.unaryInters...)
 	}
 
-	if len(mo.streamInters)>0 {
-		stream = grpc_middleware.ChainStreamServer(mo.streamInters...) 
+	if len(mo.streamInters) > 0 {
+		stream = grpc_middleware.ChainStreamServer(mo.streamInters...)
 	}
 
-	return unary,stream
+	return unary, stream
 }
--- middleware/middleware_painc.go
+++ middleware/middleware_painc.go
@@ -3,18 +3,29 @@ package middleware
 import (
 	"errors"
 	"fmt"
+
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
-func handlePainc (ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, rerr error)   {
+func handlePainc(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, rerr error) {
 	defer func() {
 		if err := recover(); err != nil {
-			resp = nil
-			rerr = errors.New(fmt.Sprintf("%v", err)) 
+			err = errors.New(fmt.Sprintf("%v", err))
 			fmt.Println(err)
 		}
 	}()
 
 	return handler(ctx, req)
+}
+
+func hanlderStreamPainc(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	defer func() {
+		if err := recover(); err != nil {
+			err = errors.New(fmt.Sprintf("%v", err))
+			fmt.Println(err)
+		}
+	}()
+
+	return handler(srv, ss)
 }
