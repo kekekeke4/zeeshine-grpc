cd76b0454ae72171cacdabfe7f7a5fede18cf869
zengke <kez@zengkedeMac-mini.local>
2019-03-19 20:05:51 +0800

    添加测试代码

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
-import(
+import (
 	"github.com/kekekeke4/zeeshine-grpc/middleware"
 	"github.com/kekekeke4/zeeshine-grpc/server"
 	"google.golang.org/grpc"
 )
 
-func main()  {
-	sb:= server.NewServerBuilder()
-		sv:= sb.Build(func(so *server.ServerOptions){
-			so.Initialize(5150,func(mco *middleware.MiddlewareChainOptions) (grpc.UnaryServerInterceptor,grpc.StreamServerInterceptor){
-				return	mco.UsePaincMiddleware().
-							UseOAuthMiddleware().
-							BuildMiddleware()
+func main() {
+	sb := server.NewServerBuilder()
+	sv := sb.Build(func(so *server.ServerOptions) {
+		so.Initialize(5150, func(mco *middleware.MiddlewareChainOptions) (grpc.UnaryServerInterceptor, grpc.StreamServerInterceptor) {
+			return mco.UsePaincMiddleware().
+				UseOAuthMiddleware().
+				BuildMiddleware()
+		}).
+			UseServices(func(grpcServer *grpc.Server) {
 			}).
-			UseServices(func(grpcServer *grpc.Server){
-			}).
-			UseConsulRegister("addr","serviceName",nil)
-		})
-		sv.Serve()
+			UseConsulRegister("192.168.2.1", "serviceName", nil)
+	})
+	sv.Serve()
 }
